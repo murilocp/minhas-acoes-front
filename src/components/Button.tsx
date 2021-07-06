a7a50f3cf7387d7e216f260e15652be025941ea0
Murilo Picinato <murilo@sysplan.com.br>
2021-07-05 23:35:38 -0300

    delete button

--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { ButtonContainer } from '../styles/components/Button';
-
-export const Button: React.FC = ({ children }) => {
-  return <ButtonContainer type="button">{children}</ButtonContainer>;
-};
