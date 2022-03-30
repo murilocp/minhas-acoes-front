2807fb7a7e9d76e473b583f800d31a80bd5607a4
Murilo Picinato <murilo@combudata.com>
2022-03-29 23:33:08 -0300

    Ajustando config

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -23,23 +23,19 @@ export default function SEO({
     !shouldExcludeTitleSuffix ? '| Minhas Ações' : ''
   }`;
 
-  const pageUrl = `https://minhasacoes.com.br`;
+  const pageUrl = `https://minhasacoes.com.br/${url}`;
 
   return (
     <Head>
       <title>{pageTitle}</title>
-          
       <meta charSet="UTF-8" />
       <meta name="language" content="pt-BR" />
-          
       <meta name="image" content={image} />
       <meta name="description" content={description} />
-          
       <meta name="robots" content="all" />
-          
       <meta name="author" content="Autor" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          
+
       {isArticle ? (
         <meta property="og:type" content="article" key="type" />
       ) : (
@@ -48,10 +44,10 @@ export default function SEO({
       <meta property="og:url" content={pageUrl} />
       <meta property="og:title" content={pageTitle} />
       <meta property="og:image" content={image} />
-       
       <meta property="og:description" content={description} />
+
       <meta property="article:author" content="Autor" />
-          
+
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:description" content={description} />
